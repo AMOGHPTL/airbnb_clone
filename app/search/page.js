c61dc8e1ceaf0093627b0117d0850e24d00477de
AMOGHPTL <amoghp549@gmail.com>
2025-02-10 01:13:10 +0530

    creating search page and buttons in search page

---
+++ app/search/page.js
@@ -0,0 +1,30 @@
+import React from 'react'
+import Header from '../components/Header'
+import Footer from '../components/Footer'
+
+function Search() {
+  return (
+    <div>
+      <Header/>
+
+      <main className='flex'>
+        <section className='px-10'>
+         <p className='text-xs text-gray-600 font-semibold'>300+ Stays for 5 guests</p>
+         <h1 className='text-3xl font-semibold mt-2 mb-6'>Stays in London</h1>
+         <div className='hidden md:flex space-x-10 px-3 mt-8  text-sm text-gray-600 font-semibold'>
+            <p className='button'>Cancellation flexibilty</p>
+            <p className='button'>Types of place</p>
+            <p className='button'>Price</p>
+            <p className='button'>Room & beds</p>
+            <p className='button'>More filters</p>
+         </div>
+        </section>
+      </main>
+
+
+      <Footer/>
+    </div>
+  )
+}
+
+export default Search
